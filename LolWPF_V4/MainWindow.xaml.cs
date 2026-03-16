24461019113876c15a5b83d874fc1f8d896738ad
Cs12krisz <csigerkrisztian@gmail.com>
2026-03-16 10:27:39 +0100

    Kód egyszerűsítése

--- LolWPF_V4/MainWindow.xaml.cs
+++ LolWPF_V4/MainWindow.xaml.cs
@@ -23,34 +23,14 @@ namespace LolWPF_V4
         public MainWindow()
         {
             InitializeComponent();
-            Program.Feladat1();
-            dtgHosok.ItemsSource = Program.hosok;
-            cbxOszlopok.ItemsSource = new string[] { "Name", "Title", "Category", "Tag", "HP", "Attackdamage", "Attackdamageperlevel" };
+
         }
 
         private void cbxOszlopok_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            switch (cbxOszlopok.SelectedItem)
-            {
-                case "Name":
+            string propertyName = cbxOszlopok.SelectedItem.ToString();
 
-                    dtgHosok.ItemsSource = Program.hosok.OrderByDescending(x => x.Name); break;
-                case "Title":
-                    dtgHosok.ItemsSource = Program.hosok.OrderByDescending(x => x.Title); break;
-                case "Category":
-                    dtgHosok.ItemsSource = Program.hosok.OrderByDescending(x => x.Category); break;
-                case "Tag":
-                    dtgHosok.ItemsSource = Program.hosok.OrderByDescending(x => x.Tag); break;
-                case "HP":
-                    dtgHosok.ItemsSource = Program.hosok.OrderByDescending(x => x.HP); break;
-                case "Attackdamage":
-                    dtgHosok.ItemsSource = Program.hosok.OrderByDescending(x => x.Attackdamage); break;
-                case "Attackdamageperlevel":
-                    dtgHosok.ItemsSource = Program.hosok.OrderByDescending(x => x.Attackdamageperlevel); break;
-            }
-
-
-
+            dtgHosok.ItemsSource = Program.hosok.OrderByDescending(x => x.GetType().GetProperty(propertyName).GetValue(x));
 
         }
 
@@ -62,17 +42,34 @@ namespace LolWPF_V4
 
         private void btMentes_Click(object sender, RoutedEventArgs e)
         {
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.FileName = $"{cbxOszlopok.SelectedValue}.txt";
-            if (saveFileDialog.ShowDialog() == true)
+            try
             {
-                StreamWriter streamWriter = new StreamWriter(saveFileDialog.FileName);
-                foreach (var item in dtgHosok.ItemsSource as IOrderedEnumerable<Hos>)
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.FileName = $"{cbxOszlopok.SelectedValue}.txt";
+                if (saveFileDialog.ShowDialog() == true)
                 {
-                    streamWriter.WriteLine($"{item.Name};{item.Title};{item.Category};{item.Tag};{item.HP};{item.Attackdamage};{item.Attackdamageperlevel}");
+                    StreamWriter streamWriter = new StreamWriter(saveFileDialog.FileName);
+                    foreach (var item in dtgHosok.ItemsSource as IOrderedEnumerable<Hos>)
+                    {
+                        streamWriter.WriteLine($"{item.Name};{item.Title};{item.Category};{item.Tag};{item.HP};{item.Attackdamage};{item.Attackdamageperlevel}");
+                    }
+                    streamWriter.Close();
+                    MessageBox.Show("Sikeres mentés");
                 }
-                streamWriter.Close();
+
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Program.Feladat1();
+            dtgHosok.ItemsSource = Program.hosok;
+            cbxOszlopok.ItemsSource = new string[] { "Name", "Title", "Category", "Tag", "HP", "Attackdamage", "Attackdamageperlevel" };
         }
     }
 }
